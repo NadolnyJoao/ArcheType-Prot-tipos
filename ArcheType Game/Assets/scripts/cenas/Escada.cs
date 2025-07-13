61fe5cdecbbd953f9533adaa2e8776f3d364faf2
PedroVitor-oss <pedro.santos22@escola.pr.gov.br>
2025-07-13 19:02:14 -0300

    confg inputsystem nas senas e arumado a animação do load

--- ArcheType Game/Assets/scripts/cenas/Escada.cs
+++ ArcheType Game/Assets/scripts/cenas/Escada.cs
@@ -6,6 +6,7 @@ public class Escada : MonoBehaviour
 {
     public Transform[] portas;
     public GameObject Player;
+    private int currentPortaIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +22,19 @@ public class Escada : MonoBehaviour
 
     public void GotoPorta(int index )
     {
-        if (index < 0 || index >= portas.Length)
+        currentPortaIndex = index;
+       Invoke("GotoPortaHowSleep", 0.5f);
+    }
+    private void GotoPortaHowSleep()
+    {
+        if (currentPortaIndex < 0 || currentPortaIndex >= portas.Length)
         {
             Debug.LogError("Index out of range for portas array.");
             return;
         }
 
-        Player.transform.position = portas[index].position;
-        Player.transform.rotation = portas[index].rotation;
+        Player.transform.position = portas[currentPortaIndex].position;
+        Player.transform.rotation = portas[currentPortaIndex].rotation;
 
         // Optional: If you want to reset the player's velocity or other properties, you can do that here.
         Rigidbody playerRigidbody = Player.GetComponent<Rigidbody>();
