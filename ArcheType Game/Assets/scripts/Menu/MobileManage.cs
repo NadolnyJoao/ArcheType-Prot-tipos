2e60718ec0c69a5aaff51fd8793b37bdd05ebc2f
PedroVitor-oss <pedro.santos22@escola.pr.gov.br>
2025-07-15 10:34:34 -0300

    add Mobile manage e seu esprite para joistick

---
+++ ArcheType Game/Assets/scripts/Menu/MobileManage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mobilePainel;
+
+    void Start()
+    {
+        mobilePainel.SetActive(false);
+
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            mobilePainel.SetActive(true);
+            Debug.Log("Mobile interface enabled.");
+        }
+        else
+        {
+            Debug.Log("Not a mobile platform, mobile interface disabled.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
