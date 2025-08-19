faa49f579dd9e25d3dbfd43df5d07fc5f5773cca
PedroVitor-oss <pedro.santos22@escola.pr.gov.br>
2025-08-19 16:59:31 -0300

    criado fase renascentista

--- ArcheType Game/Assets/scripts/player/PlayerMoviment.cs
+++ ArcheType Game/Assets/scripts/player/PlayerMoviment.cs
@@ -37,6 +37,7 @@ public class PlayerMoviment : MonoBehaviour
         ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>(); 
+        if(dialogoSystenUI==null)
         dialogoSystenUI = DialogoSystenUI.dialogoSystenUI;
         Debug.Log("dialogo system fund is : " + dialogoSystenUI);
     }
