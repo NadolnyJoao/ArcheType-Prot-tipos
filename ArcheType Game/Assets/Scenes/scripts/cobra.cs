247a5c34cb2d2f5253cd4991ff02cc2ccea03275
Keleson <jonasreserva1@gmail.com>
2025-08-26 17:00:26 -0300

    cobra (ainda falta o sprite)
    mecanica do egito

---
+++ ArcheType Game/Assets/Scenes/scripts/cobra.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cobra : MonoBehaviour
+{
+    public Transform PlayerPosition;
+    public float speed = 3.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
