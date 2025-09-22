7fce0e721c59974ff233ff8a4a7b94a2a286c966
Keleson <153941406+Keleson@users.noreply.github.com>
2025-09-22 07:43:58 -0300

    chao do egito caindo e voltando
    ajeitei o script e so falta ajeitar o puzzle do egito

--- ArcheType Game/Assets/scripts/GroundFall/FallingGround.cs
+++ ArcheType Game/Assets/scripts/GroundFall/FallingGround.cs
@@ -3,31 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingGround : MonoBehaviour
-{   
+{
+    [Header("Tempos de execução")]
     public float timeToFall = 1.0f;
-    float timeToDestroy = 0.5f;
-    private Rigidbody2D rg; 
-    // Start is called before the first frame update
+    public float timeToDestroy = 2.0f;
+    public float timeToBack = 3.0f;
+
+    private Rigidbody2D rg;
+    public GameObject groundPrefab; 
+
+    private Quaternion startRotation;
+    private Vector2 startPosition;
+
     void Start()
     {
-        rg = GetComponent<Rigidbody2D>(); 
+        rg = GetComponent<Rigidbody2D>();
+
+        // Se não achar Rigidbody2D, evita erro
+        if (rg == null)
+        {
+            Debug.LogError("Sem RigidBody nesta bomba xd");
+            return;
+        }
+
         rg.isKinematic = true; 
+        startPosition = transform.position;
+        startRotation = transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D col)
     {
-        
-    }
-    void OnCollisionEnter2D(Collision2D col){
-        if(col.gameObject.CompareTag("Player")){
-            Debug.Log("OIe"); 
-            Invoke("Cair",timeToFall); 
+        if (col.gameObject.CompareTag("Player"))
+        {
+            
+            Invoke(nameof(Cair), timeToFall);
         }
     }
-    void Cair(){
+
+    void Cair()
+    {
         rg.isKinematic = false;
+        Invoke(nameof(VoltarChao), timeToBack); // só instancia após cair
         Destroy(this.gameObject, timeToDestroy); 
-        
+    }
+
+    void VoltarChao()
+    {
+        Instantiate(groundPrefab, startPosition, startRotation);
     }
 }
