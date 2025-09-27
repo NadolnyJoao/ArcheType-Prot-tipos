0ec813e22efa194dccf844a4f61b8cecda3921f4
PedroVitor-oss <pedro.santos22@escola.pr.gov.br>
2025-09-27 17:32:56 -0300

    add limits de camera em cenarios feachados
    teste na fase renascentsitasalva

--- ArcheType Game/Assets/scripts/Camera/CameraFollow.cs
+++ ArcheType Game/Assets/scripts/Camera/CameraFollow.cs
@@ -9,6 +9,10 @@ public class CameraFollow : MonoBehaviour
     public bool delay = false;
     public float speed = 1;
 
+    [Header("Limits")]
+    public List<Transform> limites = new List<Transform>();
+
+
     private void Start()
     {
         offset.z = -10;
@@ -16,5 +20,30 @@ public class CameraFollow : MonoBehaviour
     private void Update()
     {
         transform.position = playerTransform.position + offset;
+
+        foreach (var limite in limites)
+        {
+            Vector3 nposition = limite.position;
+            nposition.z = transform.position.z;
+            float dist = Vector3.Distance(transform.position, nposition);
+            Debug.Log("dist cam to limit " + dist);
+            if (dist < 7)
+            {
+                if (limite.name.Contains("RightLimit"))
+                {
+                    if (limite.position.x < transform.position.x)
+                    {
+                        // Vector3 direction = (transform.position - limite.position).normalized;
+                        transform.position = new Vector3(limite.position.x, transform.position.y, transform.position.z);
+                    }
+                }
+                else if (limite.position.x > transform.position.x)
+                {
+                    // Vector3 direction = (transform.position - limite.position).normalized;
+                    transform.position = new Vector3(limite.position.x, transform.position.y, transform.position.z);
+                }
+            }
+        }
+
     }
 }
