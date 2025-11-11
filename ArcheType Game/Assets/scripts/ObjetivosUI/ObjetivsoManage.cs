cac179cff5f9a62331512c06b2324f21305d9adf
20223021439-cell <pedro.santos22@escola.pr.gov.br>
2025-11-11 11:38:38 -0300

    lista de objetivos quase tudo pronto

---
+++ ArcheType Game/Assets/scripts/ObjetivosUI/ObjetivsoManage.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ObjetivsoManage : MonoBehaviour
+{
+
+    public TMP_Text textoMainObjt;
+    public List<TMP_Text> textsobjetivos = new List<TMP_Text>();
+    public List<string> objetivos = new List<string>();
+    public Animator ani;
+    public void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    public void AddObjetivo(string objt)
+    {
+        objetivos.Add(objt);
+        SetObjtUI();
+    }
+    private void SetObjtUI()
+    {
+        textoMainObjt.text = objetivos[objetivos.Count - 1];
+
+        for( int i = 0; i <4; i++)
+        {
+            if(objetivos[objetivos.Count - 1 - i] != null)
+            {
+
+                textsobjetivos[i].text = objetivos[objetivos.Count - 1 - i];
+            }else{
+                textsobjetivos[i].text = "Ta vivo";
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    public void Expand()
+    {
+        Debug.Log("expandi objetivo");
+        ani.SetTrigger("change");
+    }
+}
